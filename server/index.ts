63497f1797bc4ecc62d0095d8bba71d645525378
kishan601 <kk4733031@gmail.com>
2025-04-10 01:44:51 +0530

    fixing server issues

--- server/index.ts
+++ server/index.ts
@@ -61,8 +61,7 @@ app.use((req, res, next) => {
   const port = 3001;
   server.listen({
     port,
-    host: "0.0.0.0",
-    reusePort: true,
+    host: "localhost",
   }, () => {
     log(`serving on port ${port}`);
   });
